8dd207a621d462bb67eef1a469ddb4944f8c1284
yuriikrevnyi <yuriikrevnyi@gmail.com>
2017-01-11 17:35:11 +0200

    Cleanup

--- dist/notifiers/debugErrorNotifier.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.default = debugErrorNotifier;
-function debugErrorNotifier(err, attempt) {
-  console.log("Attempt #" + attempt + " failed with error: " + err);
-}
