c21b3091788a88caeeabf2e7d499df6827b1b9bd
yuriikrevnyi <yuriikrevnyi@gmail.com>
2017-01-13 08:55:50 +0200

    wip

---
+++ examples/basic/index.js
@@ -0,0 +1,12 @@
+import Promise from 'bluebird';
+import retry from 'retryer';
+
+// STEP 1: create function that returns promise
+function sendRequest() {
+   return request('http://site.com/')
+}
+
+// STEP 2: Pass that function to the retry(FUNCTION_NAME)
+retry(sendRequest)
+  .then(data => console.log('Connected 🎉'))
+  .catch(error => console.log('error'))
