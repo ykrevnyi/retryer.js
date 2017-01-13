1701dbe8d4a423ea35a0f607fbfdf28e04bff4b3
yuriikrevnyi <yuriikrevnyi@gmail.com>
2017-01-13 10:40:02 +0200

    diff

--- examples/basic/index.js
+++ examples/basic/index.js
@@ -1,22 +1,7 @@
 import request from 'request-promise';
 import retry from 'retryer';
 
-// STEP 1: create function that returns promise
-function sendRequest() {
-   return request('http://site.com/')
-}
 
-// STEP 2: Pass that function to the retry(FUNCTION_NAME)
-// Notice that we pass `sendRequest` without brackets `sendRequest()`
-// ✅ (Correct) retry(sendRequest)
-// ❌ (Wrong)   retry(sendRequest())
-retry(sendRequest)
+request('http://site.com/')
   .then(data => console.log('Connected 🎉'))
   .catch(error => console.log('error'))
-
-
-// BTW
-// Here is how your code looks like without `retryer`
-// request('http://site.com/')
-//   .then(data => console.log('Connected 🎉'))
-//   .catch(error => console.log('error'))
