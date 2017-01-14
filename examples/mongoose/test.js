4008c9f428165c5eaa4f53585a525e73b162e156
yuriikrevnyi <yuriikrevnyi@gmail.com>
2017-01-14 15:22:59 +0200

    wip

--- examples/mongoose/test.js
+++ examples/mongoose/test.js
@@ -1,6 +1,13 @@
+mongoose.Promise = Promise;
 
-mongoose.connect('mongodb://mongodb/test-db');
+function mongooseConnect() {
+  return mongoose.connect('mongodb://mongodb/test-db');
+}
 mongoose.connection.on('error', console.log('error'));
 mongoose.connection.once('open', function() {
   console.log('Connected 🎉');
 });
+
+retry(mongooseConnect)
+  .then(data => console.log('Connected 🎉'))
+  .catch(err => console.log('error'););
