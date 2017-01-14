9d7dc6870345f89ea2ff9e3a2fbb1f3ecbb24636
yuriikrevnyi <yuriikrevnyi@gmail.com>
2017-01-14 13:36:57 +0200

    wip

--- examples/advanced-options/index.js
+++ examples/advanced-options/index.js
@@ -1,21 +1,10 @@
 import request from 'request-promise';
 import retry from 'retryer';
 
-function failingPromise(totaFails) {
-  let iteration = 0;
-
-  return function getPromise() {
-    if (iteration++ >= totaFails) {
-      return Promise.resolve('success');
-    }
-
-    return Promise.reject(`Cannot connect to site.com`);
-  }
-}
-
 function sendRequest() {
-   return request('http://non-existing-resource.com/');
-  //  return request('http://google.com/');
+  //  return request('http://non-existing-resource.com/');
+  // OR
+  return request('http://google.com/');
 }
 
 const options = {
@@ -26,25 +15,9 @@ const options = {
   onError
 };
 
-console.log('**********************************');
-console.log('*** Example 1: Failing promise ***');
-console.log('**********************************');
 retry(sendRequest, options)
   .then(data => console.log('🛰 Gliding the space'))
-  .catch(error => {
-    console.log('🚧 Sorry mate, the rocket is broken. You cannot fly to the moon ATM')
-
-    setTimeout(function() {
-      console.log('\n\n*************************************');
-      console.log('*** Example 2: Successful promise ***');
-      console.log('*************************************');
-    }, 2500)
-
-    setTimeout(function() {
-      retry(failingPromise(1), options)
-        .then(data => console.log('🛰 Gliding the space'))
-    }, 3000)
-  })
+  .catch(error => console.log('🚧 Sorry mate, the rocket is broken. You cannot fly to the moon ATM'))
 
 
 // Helper functions
@@ -61,8 +34,5 @@ function onError(err, attempt) {
     return console.log(`📛 📛 📛 Oh, crap! Retrying the last time 🚦`);
   }
 
-  console.log(`📛 📛 📛 Something went wrong. Restarting engine in 2sec 🚦`);
+  console.log(`📛 📛 📛 Oh, crap! Something went wrong. Restarting engine in 2sec 🚦`);
 }
-
-
-setTimeout(() => {}, 20000000)
