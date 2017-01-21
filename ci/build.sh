53296d4b0218b05f574b0ae38b502df03ae8604f
yuriikrevnyi <yuriikrevnyi@gmail.com>
2017-01-21 16:03:10 +0200

    debug shippable

--- ci/build.sh
+++ ci/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-sudo docker build \
-  -t $DOCKER_IMAGE_NAME:$BRANCH.$BUILD_NUMBER \
-  -f container_images/Dockerfile-nodejs-v7.1.0 \
-  container_images/
+docker build \
+  -t $DOCKER_IMAGE_NAME:$DOCKER_TAG \
+  -f Dockerfile \
+  ..
