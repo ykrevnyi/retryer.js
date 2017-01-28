d5d4fe1b7437630504f98046ad1cb37e0e52bab7
yuriikrevnyi <yuriikrevnyi@gmail.com>
2017-01-28 16:59:49 +0200

    wip

--- ci/publish.sh
+++ ci/publish.sh
@@ -6,7 +6,7 @@ docker push $DOCKER_IMAGE_NAME:$DOCKER_TAG
 # Publish npm package
 if [ "$IS_RELEASE" = true ]; then
   echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" > ~/.npmrc
-  chmod 0600 .npmrc
+  chmod 0600 ~/.npmrc
   cat ~/.npmrc
   npm --no-git-tag-version version $DOCKER_TAG
   npm publish
